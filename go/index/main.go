3a5e028d7a0a11bce31fa2d1d2bf11c97e63af37
fizzzmath <stasha0601@gmail.com>
2025-06-01 18:46:31 +0300

    Add files via upload

--- go/index/main.go
+++ go/index/main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/cgi"
-	"os"
+	"text/template"
 )
 
 func main() {
 	cgi.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// tmpl, err := template.ParseFiles("templates/base.html", "template/home.html")
+		tmpl, err := template.ParseFiles("/templates/base.html", "/templates/home.html")
 
-		// if err != nil {
-		// 	fmt.Fprintf(w, "Template error")
-		// 	log.Print(err)
-		// 	return
-		// }
+		if err != nil {
+			fmt.Fprintf(w, "Template error")
+			log.Print(err)
+			return
+		}
 
-		// tmpl.ExecuteTemplate(w, "home.html", nil)
-
-		fmt.Fprintf(w, "%s", os.Getenv("DOCUMENT_ROOT"))
+		tmpl.ExecuteTemplate(w, "home.html", nil)
 	}))
 }
