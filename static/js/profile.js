5148856803f87583f4ffe36949b053f99391251e
fizzzmath <stasha0601@gmail.com>
2025-06-02 20:38:41 +0300

    Fix

--- static/js/profile.js
+++ static/js/profile.js
@@ -36,7 +36,7 @@ function handleProfileUpdate(e) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            // 'Authorization': `Bearer ${localStorage.getItem('token')}`
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
         },
         body: JSON.stringify(data)
     })
